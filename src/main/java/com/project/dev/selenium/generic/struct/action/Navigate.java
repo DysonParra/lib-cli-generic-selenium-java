5fbcd13ce0baeecaa8986b6b0afa55805e83af8e
unknown <dysontilano@gmail.com>
2023-11-01 15:01:28 -0500

    Added Action to navigate

---
+++ src/main/java/com/project/dev/selenium/generic/struct/action/Navigate.java
@@ -0,0 +1,71 @@
+/*
+ * @fileoverview    {WaitPageLoad}
+ *
+ * @version         2.0
+ *
+ * @author          Dyson Arley Parra Tilano <dysontilano@gmail.com>
+ *
+ * @copyright       Dyson Parra
+ * @see             github.com/DysonParra
+ *
+ * History
+ * @version 1.0     Implementation done.
+ * @version 2.0     Documentation added.
+ */
+package com.project.dev.selenium.generic.struct.action;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.dev.selenium.generic.struct.Action;
+import java.time.Duration;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * TODO: Description of {@code WaitPageLoad}.
+ *
+ * @author Dyson Parra
+ * @since 11
+ */
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Navigate extends Action {
+
+    @JsonProperty(value = "url")
+    protected String url;
+    @JsonProperty(value = "timeout")
+    protected Integer timeout;
+
+    /**
+     * Ejecuta una acción en el elemento de la página actual.
+     *
+     * @param driver  es el driver del navegador.
+     * @param element es el {@code WebElement} que se le va a ejecutar dicha acción.
+     * @param flagsMap contiene las {@code Flag} pasadas por consola.
+     * @return {@code true} si se ejecuta la acción correctamente.
+     * @throws Exception si ocurre algún error ejecutando la acción indicada.
+     */
+    @Override
+    public boolean executeAction(@NonNull WebDriver driver, @NonNull WebElement element, Map<String, String> flagsMap) throws Exception {
+        //System.out.println("Navigate");
+        driver.get(url);
+        new WebDriverWait(driver, Duration.ofMillis(timeout))
+                .until((WebDriver webDriver) -> ((JavascriptExecutor) webDriver)
+                .executeScript("return document.readyState")
+                .equals("complete"));
+        //System.out.println("Page loaded.");
+        return true;
+    }
+
+}
