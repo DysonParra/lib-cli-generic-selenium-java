04ef574f226552ce812e3fa27c9116a9804af2ff
unknown <dysontilano@gmail.com>
2024-08-20 13:05:32 -0500

    Added action to wait until a dom element is present

---
+++ src/main/java/com/project/dev/selenium/generic/struct/action/WaitElementExist.java
@@ -0,0 +1,69 @@
+/*
+ * @fileoverview    {WaitElementState}
+ *
+ * @version         2.0
+ *
+ * @author          Dyson Arley Parra Tilano <dysontilano@gmail.com>
+ *
+ * @copyright       Dyson Parra
+ * @see             github.com/DysonParra
+ *
+ * History
+ * @version 1.0     Implementation done.
+ * @version 2.0     Documentation added.
+ */
+package com.project.dev.selenium.generic.struct.action;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.dev.selenium.generic.struct.Action;
+import java.time.Duration;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * TODO: Description of {@code WaitElementState}.
+ *
+ * @author Dyson Parra
+ * @since 11
+ */
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WaitElementExist extends Action {
+
+    @JsonProperty(value = "timeout")
+    protected Integer timeout;
+    @JsonProperty(value = "element-xpath")
+    protected String elementXpath;
+
+    /**
+     * Ejecuta una acción en el elemento de la página actual.
+     *
+     * @param driver   es el driver del navegador.
+     * @param element  es el {@code WebElement} que se le va a ejecutar dicha acción.
+     * @param flagsMap contiene las {@code Flag} pasadas por consola.
+     * @return {@code true} si se ejecuta la acción correctamente.
+     * @throws Exception si ocurre algún error ejecutando la acción indicada.
+     */
+    @Override
+    public boolean executeAction(@NonNull WebDriver driver, @NonNull WebElement element, Map<String, String> flagsMap) throws Exception {
+        new WebDriverWait(driver, Duration.ofMillis(timeout))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(elementXpath)));
+        return true;
+    }
+
+}
