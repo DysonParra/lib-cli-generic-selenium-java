a1f14e601021c3f9384db70cf26d555325cdd87e
unknown <dysontilano@gmail.com>
2026-04-10 16:10:55 -0500

    feat: Added Action saveTextToFile

---
+++ src/main/java/com/project/dev/selenium/generic/struct/action/SaveTextToFile.java
@@ -0,0 +1,84 @@
+/*
+ * @overview        {SaveTextToFile}
+ *
+ * @version         2.0
+ *
+ * @author          Dyson Arley Parra Tilano <dysontilano@gmail.com>
+ *
+ * @copyright       Dyson Parra
+ * @see             github.com/DysonParra
+ *
+ * History
+ * @version 1.0     Implementation done.
+ * @version 2.0     Documentation added.
+ */
+package com.project.dev.selenium.generic.struct.action;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.dev.selenium.generic.struct.Action;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * TODO: Description of {@code SaveTextToFile}.
+ *
+ * @author Dyson Parra
+ * @since Java 17 (LTS), Gradle 7.3
+ */
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SaveTextToFile extends Action {
+
+    @JsonProperty(value = "text")
+    protected String text;
+    @JsonProperty(value = "output-path")
+    protected String outputPath;
+    @JsonProperty(value = "output-file-name")
+    protected String outputFileName;
+
+    /**
+     * Ejecuta una acción en el elemento de la página actual.
+     *
+     * @param driver        es el driver del navegador.
+     * @param element       es el {@code WebElement} que se le va a ejecutar dicha acción.
+     * @param flagsMap      contiene las {@code Flag} pasadas por consola.
+     * @param scriptResults contiene los {@code Object} obtenidos de cada script ejecutado.
+     * @return {@code true} si se ejecuta la acción correctamente.
+     * @throws Exception si ocurre algún error ejecutando la acción indicada.
+     */
+    @Override
+    public boolean executeAction(@NonNull WebDriver driver, @NonNull WebElement element, Map<String, String> flagsMap, List<Object> scriptResults) throws Exception {
+        text = Action.assignScriptResult(text, scriptResults);
+        outputPath = Action.assignScriptResult(outputPath, scriptResults);
+        outputFileName = Action.assignScriptResult(outputFileName, scriptResults);
+        System.out.println("        Current page: " + driver.getCurrentUrl());
+        new File(outputPath).mkdirs();
+        try (FileOutputStream fos = new FileOutputStream(outputPath + "\\" + outputFileName, true);
+                OutputStreamWriter osr = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+                BufferedWriter writer = new BufferedWriter(osr);) {
+            writer.write(text);
+        } catch (Exception e) {
+            throw e;
+        }
+        return true;
+    }
+
+}
