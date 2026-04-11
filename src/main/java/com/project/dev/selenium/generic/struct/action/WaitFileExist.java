4a34098a35f0e29247230305c52058d17e3ebbc8
unknown <dysontilano@gmail.com>
2026-04-11 16:13:46 -0500

    feat: Added Action WaitFileExist

---
+++ src/main/java/com/project/dev/selenium/generic/struct/action/WaitFileExist.java
@@ -0,0 +1,81 @@
+/*
+ * @overview        {WaitFileExist}
+ *
+ * @version         2.0
+ *
+ * @author          Dyson Arley Parra Tilano <dysontilano@gmail.com>
+ *
+ * @copyright       Dyson Parra
+ * @see             github.com/DysonParra
+ *
+ * History
+ * @version 1.0     Implementation done.
+ * @version 2.0     Documentation added.
+ */
+package com.project.dev.selenium.generic.struct.action;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.dev.file.generic.FileProcessor;
+import com.project.dev.selenium.generic.struct.Action;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * TODO: Description of {@code WaitFileExist}.
+ *
+ * @author Dyson Parra
+ * @since Java 17 (LTS), Gradle 7.3
+ */
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WaitFileExist extends Action {
+
+    @JsonProperty(value = "filepath")
+    protected String filePath;
+    @JsonProperty(value = "validation-gap")
+    protected Integer validationGap;
+    @JsonProperty(value = "timeout")
+    protected Integer timeout;
+
+    /**
+     * Ejecuta una acción en el elemento de la página actual.
+     *
+     * @param driver        es el driver del navegador.
+     * @param element       es el {@code WebElement} que se le va a ejecutar dicha acción.
+     * @param flagsMap      contiene las {@code Flag} pasadas por consola.
+     * @param scriptResults contiene los {@code Object} obtenidos de cada script ejecutado.
+     * @return {@code true} si se ejecuta la acción correctamente.
+     * @throws Exception si ocurre algún error ejecutando la acción indicada.
+     */
+    @Override
+    public boolean executeAction(@NonNull WebDriver driver, @NonNull WebElement element, Map<String, String> flagsMap, List<Object> scriptResults) throws Exception {
+        filePath = Action.assignScriptResult(filePath, scriptResults);
+        int elapsedTime = 0;
+        timeout = Integer.valueOf(Action.assignScriptResult(timeout, scriptResults));
+        while(!FileProcessor.validateFile(filePath) && elapsedTime <= timeout) {
+          if (validationGap < timeout) {
+              elapsedTime += validationGap;
+              Thread.sleep(validationGap);
+          } else {
+              Thread.sleep(timeout);
+          }
+        }
+        return true;
+    }
+
+}
